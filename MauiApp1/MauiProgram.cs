0febba93b1edce9473912379958b69474e70161f
Usuario <Usuario@CPD03-M18>
2024-02-22 14:48:08 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiApp1
+{
+    public static class MauiProgram
+    {
+        <?xml version = "1.0" encoding = "UTF-8" ?>
+<Application xmlns = "http://schemas.microsoft.com/dotnet/2021/maui"
+             xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
+             xmlns:local="clr-namespace:MauiPrimeiroApp2"
+             x:Class="MauiPrimeiroApp2.App">
+    <Application.Resources>
+        <ResourceDictionary>
+            <ResourceDictionary.MergedDictionaries>
+                <ResourceDictionary Source = "Resources/Styles/Colors.xaml" />
+                < ResourceDictionary Source="Resources/Styles/Styles.xaml" />
+            </ResourceDictionary.MergedDictionaries>
+        </ResourceDictionary>
+    </Application.Resources>
+</Application>
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
